8b52b0bd8e24c4108e2c8a6ccab77a56a2af195e
jaren <jarensmart@yahoo.com>
2020-06-28 20:56:28 -0400

    Added GET && POST for resources

--- models/resource.js
+++ models/resource.js
@@ -1,3 +1,22 @@
 const db = require("../data/config");
 
-module.exports = {};
+function findById(id) {
+  return db("projects").where({ id });
+}
+
+function findResources(id) {
+  return db("resources")
+    .innerJoin("projects as p", "p.id", "resources.project_id")
+    .select("p.name", "resources.name", "resources.description")
+    .where({ project_id: id });
+}
+
+function addResource(resource) {
+  return db("resources").insert(resource);
+}
+
+module.exports = {
+  findResources,
+  findById,
+  addResource,
+};
